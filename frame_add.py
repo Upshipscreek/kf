087a5a57d8ad4edab13e9be3046b2cd4daad0687
fatima1712 <sfatima1712@gmail.com>
2016-06-03 15:44:37 +0530

    Add files via upload

---
+++ frame_add.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+cap=cv2.VideoCapture(0)
+
+ret, frame0=cap.read()
+sum=frame0
+while(1):
+	
+	ret, frame=cap.read()
+	#sum=cv2.add(frame0,frame)
+	sum=cv2.addWeighted(frame0,0.5,frame,0.5,0)
+	frame0=sum
+		 
+	cv2.imshow('frame',frame)
+
+	cv2.imshow('sum',sum)
+	if cv2.waitKey(1) & 0xFF == ord('p'):
+		break
+	
+cap.release() 
+cv2.destroyAllWindows()
+	
